a6cc70701c7c793042bde9157b336c6896d51656
Xuan Yi <xuan.yi.2.ng@continental-corporation.com>
2024-07-25 17:17:34 +0800

    Fix navbar selection
    Fixed navbar to be able to hover and select to indicate to user which tab was
    selected.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
 import { AboutpageComponent } from './pages/aboutpage/aboutpage.component';
 import { ProjectpageComponent } from './pages/projectpage/projectpage.component';
 
+// Take note that routes should be from most specific to least as it will match whichever it can match first
 export const routes: Routes = [
     {
         path: "",
