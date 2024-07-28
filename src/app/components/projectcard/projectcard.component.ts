84a00b2a55fb0933e8f35a71c483b6c0aeefa588
Xuannie <xuanyi1603@gmail.com>
2024-07-28 23:47:50 +0800

    testing changes

--- src/app/components/projectcard/projectcard.component.ts
+++ src/app/components/projectcard/projectcard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projectcard',
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './projectcard.component.html',
   styleUrl: './projectcard.component.css'
 })
-export class ProjectcardComponent {
+export class ProjectcardComponent implements OnInit{
+    // Property to hold the desired data
 
 }
