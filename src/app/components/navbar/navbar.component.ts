c4752d10c793b23be7b435a9393896d701b8ad45
Xuan Yi <xuan.yi.2.ng@continental-corporation.com>
2024-08-02 17:20:37 +0800

    Add responsive mobile design
    Added responsive mobile view to homepage, aboutpage, and navbar.
    
    Have not implemented resposnive view to project cards, project page, tech stack icons on about me

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+    // Navbar is initially closed
+    navbarOpen = false;
+
+    // Function to show or hide the navbar
+    toggleNavbar() {
+        this.navbarOpen = !this.navbarOpen;
+    }
   
 }
