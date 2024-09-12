48a2d1297f8de7e5eddace2c508f55bd776ebd07
Xuan Yi <xuan.yi.2.ng@continental-corporation.com>
2024-09-12 16:15:21 +0800

    Removed any console log of Github Token

--- src/assets/runtime-environment.js
+++ src/assets/runtime-environment.js
@@ -2,5 +2,4 @@
     window.__env = window.__env || {};
     // Render injects environment variables at build time
     window.__env.GITHUB_ACCESS_TOKEN = 'REPLACE_WITH_GITHUB_ACCESS_TOKEN';
-    console.log('GITHUB_ACCESS_TOKEN:', window.__env.GITHUB_ACCESS_TOKEN);
 })(this);
