377f18857a8b0415a2fe17b068f4ffa3e95c4450
Xuan Yi <xuan.yi.2.ng@continental-corporation.com>
2024-09-12 15:56:04 +0800

    Test if Github token is loaded 3

--- src/assets/runtime-environment.js
+++ src/assets/runtime-environment.js
@@ -1,6 +1,6 @@
 (function (window) {
     window.__env = window.__env || {};
     // Set the environment variable from Render here
-    window.__env.GITHUB_ACCESS_TOKEN = ''; // Will be replaced by Render's environment variable
+    window.__env.GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;; // Will be replaced by Render's environment variable
     console.log('GITHUB_ACCESS_TOKEN:', window.__env.GITHUB_ACCESS_TOKEN);
 })(this);
