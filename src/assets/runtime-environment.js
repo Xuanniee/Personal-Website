4186b712229c25241f84aa77bc446d816f5ba27b
Xuan Yi <xuan.yi.2.ng@continental-corporation.com>
2024-09-12 14:32:35 +0800

    Incorporate Github Access Token

---
+++ src/assets/runtime-environment.js
@@ -0,0 +1,6 @@
+(function (window) {
+    window.__env = window.__env || {};
+    // Set the environment variable from Render here
+    window.__env.GITHUB_ACCESS_TOKEN = '{{GITHUB_ACCESS_TOKEN}}'; // Will be replaced by Render's environment variable
+})(this);
+  
