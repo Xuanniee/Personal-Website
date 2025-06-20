b2d317f8b07369e3307ce2f06c26faba2a9a5b8f
xuanying <xuanying@global.tencent.com>
2025-06-20 17:12:35 +0800

    In middle of changes

--- src/assets/runtime-environment.js
+++
@@ -1,6 +0,0 @@
-(function (window) {
-    window.__env = window.__env || {};
-    // Render injects environment variables at build time
-    // Remember must build first before we replace the Env variable since this file is found in assets after building only
-    window.__env.GITHUB_ACCESS_TOKEN = 'REPLACE_WITH_GITHUB_ACCESS_TOKEN';
-})(this);
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    githubAccessToken: '' // Leave this blank for development, or add dummy data
-  };
-  
